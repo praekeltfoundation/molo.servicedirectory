4ecc29cb1f993114b181055ec5678d88aecb48de
Matthew Beets <matthew@informationlogistics.co.za>
2016-01-20 14:15:48 +0200

    super simple call to service directory API with hardcoded auth and URL (to be extracted)

--- molo/servicedirectory/views.py
+++ molo/servicedirectory/views.py
@@ -1,3 +1,6 @@
+import base64
+import urllib2
+
 from django.shortcuts import render
 
 from django.template import loader
@@ -15,6 +18,23 @@ def home(request):
     return HttpResponse(template.render(context, request))
 
 def result_summaries(request):
+    search_term = request.GET['search']
+
+    url = 'http://0.0.0.0:8000/api/service_lookup/?keyword={0}'.format(search_term)
+
+    api_request = urllib2.Request(url)
+    base64string = base64.encodestring('{0}:{1}'.format('root', 'adminadmin')).replace('\n', '')
+    api_request.add_header("Authorization", "Basic {0}".format(base64string))
+
+    serialized_data = urllib2.urlopen(api_request).read()
+
+
     template = loader.get_template('servicedirectory/result_summaries.html')
-    context = {}
+    context = {
+        'search': search_term,
+        'result_json': serialized_data,
+    }
+
+
+
     return HttpResponse(template.render(context, request))
