2800dd0a8a6c8f25fd96867ef4d99f4f2f1ab099
Matthew Beets <matthew@informationlogistics.co.za>
2016-01-20 13:15:08 +0200

    add urls for initial pages

--- molo/servicedirectory/urls.py
+++ molo/servicedirectory/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^result-summaries/$', views.result_summaries, name='result-summaries'),
 ]
--- molo/servicedirectory/views.py
+++ molo/servicedirectory/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
+from django.template import loader
+
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('You are browsing service directory.')
+    template = loader.get_template('servicedirectory/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def home(request):
+    template = loader.get_template('servicedirectory/home.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def result_summaries(request):
+    template = loader.get_template('servicedirectory/result_summaries.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
