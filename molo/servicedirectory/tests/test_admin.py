92426af6675cd093bfc0b0200ade928ef13e0f15
Malte Meister <malte@labs.ws>
2016-07-20 11:50:36 +0200

    get admin tests to pass, add test_updating_map_location

--- molo/servicedirectory/tests/test_admin.py
+++ molo/servicedirectory/tests/test_admin.py
@@ -1,7 +1,9 @@
+import json
+
 from django.contrib.auth.models import User
 from django.contrib.gis.geos import Point
 from django.test import TestCase
-from service_directory.api.models import Country, Organisation, Category, \
+from molo.servicedirectory.models import Country, Organisation, Category, \
     Keyword, KeywordCategory, OrganisationCategory, OrganisationKeyword
 
 
@@ -35,7 +37,9 @@ class OrganisationModelFormTestCase(TestCase):
         cls.org_cbmh = Organisation.objects.create(
             name='Netcare Christiaan Barnard Memorial Hospital',
             country=cls.country,
-            location=Point(18.418231, -33.921859, srid=4326)
+            location=json.loads(
+                Point(18.418231, -33.921859, srid=4326).geojson
+            )
         )
         cls.org_cbmh.full_clean()  # force model validation to happen
 
@@ -49,7 +53,10 @@ class OrganisationModelFormTestCase(TestCase):
         )
         ok.full_clean()  # force model validation to happen
 
-        cls.api_url = '/admin/api/organisation/{0}/'.format(cls.org_cbmh.id)
+        cls.admin_url = \
+            '/django-admin/servicedirectory/organisation/{0}/change/'.format(
+                cls.org_cbmh.id
+            )
 
     def setUp(self):
         self.client.login(username=self.SU_USERNAME, password=self.SU_PASSWORD)
@@ -70,7 +77,7 @@ class OrganisationModelFormTestCase(TestCase):
             self.org_cbmh.age_range_max else '',
             'opening_hours': self.org_cbmh.opening_hours,
             'country': self.org_cbmh.country_id,
-            'location': self.org_cbmh.location,
+            'location': json.dumps(self.org_cbmh.location),
             'facility_code': self.org_cbmh.facility_code,
 
             # fields from OrganisationCategoryInlineModelAdmin
@@ -101,6 +108,39 @@ class OrganisationModelFormTestCase(TestCase):
         }
         return data
 
+    def test_updating_map_location(self):
+        data = self.get_post_payload_for_test_organisation()
+
+        new_location = json.loads(data['location'])
+        # change the latitude
+        new_location['coordinates'][1] = -34.0
+
+        data['location'] = json.dumps(new_location)
+
+        response = self.client.post(self.admin_url, data)
+
+        self.assertEqual(302, response.status_code)
+        self.assertTrue(
+            response._headers['location'][1].endswith(
+                '/django-admin/servicedirectory/organisation/'
+            )
+        )
+
+        # if the status_code is 200 then validation for one of the fields
+        # probably failed - check the HTML response content
+        self.assertEqual(302, response.status_code)
+        self.assertTrue(
+            response._headers['location'][1].endswith(
+                '/django-admin/servicedirectory/organisation/'
+            )
+        )
+
+        org = Organisation.objects.get(pk=self.org_cbmh.id)
+        self.assertEqual(new_location['coordinates'][1],
+                         org.location['coordinates'][1])
+        self.assertEqual(new_location['coordinates'][0],
+                         org.location['coordinates'][0])
+
     def test_location_coords_field_overrides_map_location(self):
         new_lat = -33.921124
         new_lng = 18.417313
@@ -108,36 +148,36 @@ class OrganisationModelFormTestCase(TestCase):
         data = self.get_post_payload_for_test_organisation()
         data['location_coords'] = '{0},{1}'.format(new_lat, new_lng)
 
-        response = self.client.post(self.api_url, data)
+        response = self.client.post(self.admin_url, data)
 
         # if the status_code is 200 then validation for one of the fields
         # probably failed - check the HTML response content
         self.assertEqual(302, response.status_code)
         self.assertTrue(
             response._headers['location'][1].endswith(
-                '/admin/api/organisation/'
+                '/django-admin/servicedirectory/organisation/'
             )
         )
 
         org = Organisation.objects.get(pk=self.org_cbmh.id)
-        self.assertEqual(new_lat, org.location.y)
-        self.assertEqual(new_lng, org.location.x)
+        self.assertEqual(new_lat, org.location['coordinates'][1])
+        self.assertEqual(new_lng, org.location['coordinates'][0])
 
     def test_location_coords_field_validation(self):
         data = self.get_post_payload_for_test_organisation()
         data['location_coords'] = '123abc'
 
-        response = self.client.post(self.api_url, data)
+        response = self.client.post(self.admin_url, data)
 
         self.assertContains(response, 'Invalid coordinates')
 
     def test_map_location_required_if_location_coords_field_empty(self):
         data = self.get_post_payload_for_test_organisation()
-        del data['location']
+        data['location'] = ''
 
-        response = self.client.post(self.api_url, data)
+        response = self.client.post(self.admin_url, data)
 
-        self.assertContains(response, 'No geometry value provided')
+        self.assertContains(response, 'Enter valid JSON', count=2)
 
     def test_map_location_not_required_if_location_coords_supplied(self):
         new_lat = -33.921124
@@ -147,13 +187,13 @@ class OrganisationModelFormTestCase(TestCase):
         del data['location']
         data['location_coords'] = '{0},{1}'.format(new_lat, new_lng)
 
-        response = self.client.post(self.api_url, data)
+        response = self.client.post(self.admin_url, data)
 
         # if the status_code is 200 then validation for one of the fields
         # probably failed - check the HTML response content
         self.assertEqual(302, response.status_code)
         self.assertTrue(
             response._headers['location'][1].endswith(
-                '/admin/api/organisation/'
+                '/django-admin/servicedirectory/organisation/'
             )
         )
