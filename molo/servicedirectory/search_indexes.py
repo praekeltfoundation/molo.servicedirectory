85f31db3a6dd7d510d48a2153b7ef559513bb860
Malte Meister <malte@labs.ws>
2016-07-20 16:41:49 +0200

    update the location field on OrganisationIndex to use the new location_point property on the Organisation model (LocationField expects a Point object)

--- molo/servicedirectory/search_indexes.py
+++ molo/servicedirectory/search_indexes.py
@@ -5,7 +5,7 @@ from models import Organisation
 class OrganisationIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
-    location = indexes.LocationField(model_attr='location')
+    location = indexes.LocationField(model_attr='location_point')
 
     def get_model(self):
         return Organisation
